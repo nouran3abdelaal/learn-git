a5ad06dc6fef9c96f3d5c6ca2f4f9316f6ee909c
nouran3abdelaal <nourakhaled329@gmail.com>
2022-10-05 00:22:13 +0200

    2

--- test.js/add.js
+++ test.js/add.js
@@ -1,5 +1,13 @@
 console.log("hi")
+//git push -f origin main instead of git push -u origin main
+/*
+250
 
+
+NOTICE: This is never a recommended use of git. 
+This will overwrite changes on the remote. Only do this if you know 100% that your local changes should be pushed 
+to the remote main
+*/
 
 
 
